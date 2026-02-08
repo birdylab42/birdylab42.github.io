aa10156d702ef4443981b78c2ac074341f4ee7f8
Chirp Bird <birdylab42@gmail.com>
2026-02-07 20:17:12 -0500

    Finish JavaScript gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,3 +1,27 @@
+// start the image gallery
 function activateGallery() {
-    alert("Hello from the gallery file!");
+
+// select all thumbnails
+    let thumbnails  = document.querySelectorAll("#gallery-thumbs div img");
+    let mainImage   = document.querySelector("#gallery-photo img");
+    let galleryInfo = document.querySelector("#gallery-info");
+    let imageTitle  = galleryInfo.querySelector(".title");
+    let imageDesc   = galleryInfo.querySelector(".description");
+// add event listeners to the thumbnails
+thumbnails.forEach(function(thumbnail){
+    thumbnail.addEventListener("click", function() {
+        let newImageSrc   = thumbnail.dataset.largeVersion;
+        imageTitle.innerHTML = thumbnail.dataset.title;
+        imageDesc.innerHTML  = thumbnail.dataset.description;
+        // change current image indicator
+        document.querySelector(".current").classList.remove("current");
+        thumbnail.parentNode.classList.add("current");
+
+        // code to set clicked image as main image
+        mainImage.setAttribute("src", newImageSrc);
+        mainImage.setAttribute("alt", thumbnail.alt);
+
+
+    });
+});
 }
